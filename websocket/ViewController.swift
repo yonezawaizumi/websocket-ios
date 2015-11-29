83a12100d852dc6944d927d7b2e45002b33102a1
yonezawaizumi <izumi@nurs.or.jp>
2015-11-29 10:46:36 +0900

    Initial Commit

---
+++ websocket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  websocket
+//
+//  Created by yonezawaizumi on 2015/11/29.
+//  Copyright © 2015年 yonezawaizumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
